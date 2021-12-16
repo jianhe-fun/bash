3835f17a582a9865fed6a4dd96d47e16602098b1
mark <jianhe.mark@pm.me>
2021-12-16 16:38:46 +0530

    general format of case

---
+++ general.sh
@@ -0,0 +1,13 @@
+#general format of case.
+#check same folder animal.sh.
+case $SOMETHING in
+    value1)
+        statement1;
+        statement2;
+        ;;
+    value2)
+        statement3;
+        statement4;
+        ;;
+esac
+#######
