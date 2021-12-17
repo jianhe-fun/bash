857b0662112c5396e3a98f3dc367b4e0315aff85
mark <jianhe.mark@pm.me>
2021-12-17 16:57:45 +0530

    more ways to check substring exists or not

--- substring.sh
+++ substring.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This part is about substring
+#https://stackoverflow.com/questions/229551/how-to-check-if-a-string-contains-a-substring-in-bash
 echo "first way to do it"
 String='my long string';
 #1.
@@ -12,8 +13,28 @@ fi
 echo "The second way to do it"
 re='my l';
 if [[ $String =~ $re ]]; then 
-        echo "The second way to test it's here"
+        echo "#2 it's here"
 else
-        ehco "the second way to tell you it's not here";
+        ehco "#2 it's not here";
 fi
+#similar you can also check substring not exists in main string.
+
+echo "#3......"
+
+if [[ ! $String =~ $re ]]; then 
+        echo "#3 vsubstring not exists in main string."
+else
+        echo "#3 substring exists in main string.";
+fi
+#similar you can also check substring not exists in main string.
+
+echo "#4.........."
+#case "$string" in 
+#esac
+echo "#5.........."
+if  grep -q 'my' <<<"$String"; then
+        echo "it's there";
+else echo "It's not here";
+fi
+
 exit 0
