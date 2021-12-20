f2f41d054452380810479e2ba0bbce1637dd79ff
mark <jianhe.mark@pm.me>
2021-12-20 11:33:19 +0530

    using #, ##, %, %% to cut/seperate the string

--- cutstring.sh
+++ cutstring.sh
@@ -20,3 +20,29 @@ while IFS=';' read -ra ADDR; do
   done
 done <<< "$IN"
 
+#split string based on delimiter in bash.
+echo "split string based on delimiter" 
+IN="bla@some.com;john@home.com;Full Name <fulnam@other.org>"
+oIFS="$IFS"
+IFS=";"
+declare -a fields=($IN)
+IFS="$oIFS"
+unset oIFS
+echo $oIFS
+#shell way.
+IN="bla@some.com;john@home.com;Full Name <fulnam@other.org>"
+
+# ${var%%SubStr*} drops substring from first occurrence of `SubStr` to end of string
+#  ## and %% delete the longest possible matching substring.
+#   # and % delete the shortest possible matching substring from the start and end of the string respectively
+while [ "$IN" != "$iter" ] ;do
+    # extract the substring from start of string up to delimiter.
+    iter=${IN%%;*}
+    # delete this first "element" AND next separator, from $IN.
+    IN="${IN#$iter;}"
+    # Print (or doing anything with) the first "element".
+    echo "> [$iter]"
+done
+#-------------------
+
+
