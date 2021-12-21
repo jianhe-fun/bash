f07d4a0fb094dfebe0fc590b843bb977a0682831
mark <jianhe.mark@pm.me>
2021-12-21 13:33:44 +0530

    concatenate string with nested evaulation

--- concatenatestring.sh
+++ concatenatestring.sh
@@ -5,4 +5,11 @@
 declare -a a='([0]="36" [1]="18" [2]="one" [3]="word" [4]="hello world!" [5]="h
 ello world!" [6]="hello world!")'
 echo ${a[@]}
+printf -v a "%s./.%s...'%s' '%s', '%s'=='%s'=='%s'" "${a[@]}"
+echo "test....."
+echo $a
+echo "-----with evaulation nesting"
+foo="Today is:  ";
+foo=$(printf "%s %s" "$foo" "$(date)");
+echo $foo
 
